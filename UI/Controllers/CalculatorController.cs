442b6c07ae309ef3c1e5522bda1215884324ae13
saidr <saidradwanmansour@hotmail.com>
2023-10-05 21:04:04 +0200

    New UI changes

--- UI/Controllers/CalculatorController.cs
+++ UI/Controllers/CalculatorController.cs
@@ -56,53 +56,5 @@ public class CalculatorController : Controller
         ViewBag.Result = result;
         return View("Index");
     }
-    //[HttpPost]
-    //public async Task<IActionResult> Add(string numberInput)
-    //{
-    //    List<int> numbers;
-    //    try
-    //    {
-    //        numbers = numberInput.Split(',').Select(int.Parse).ToList();
-    //    }
-    //    catch (FormatException)
-    //    {
-    //        ViewBag.Error = "Invalid number format.";
-    //        return View("Index");
-    //    }
-
-    //    var client = _clientFactory.CreateClient();
-
-    //    // Konverter listen af tal til en streng, der kan inkluderes i en URL som forespørgselsparametre
-    //    var queryString = string.Join("&input=", numbers.Prepend(0)); // Bruger Prepend(0) som en hurtig måde at tilføje et element først i listen og anvende samme formattering gennem Join.
-
-    //    var result = await client.GetStringAsync($"http://adding-service/Add?{queryString}");
-
-    //    ViewBag.Result = result;
-    //    return View("Index");
-    //}
-
-    //[HttpPost]
-    //public async Task<IActionResult> Sub(string numberInput)
-    //{
-    //    List<int> numbers;
-    //    try
-    //    {
-    //        numbers = numberInput.Split(',').Select(int.Parse).ToList();
-    //    }
-    //    catch (FormatException)
-    //    {
-    //        ViewBag.Error = "Invalid number format.";
-    //        return View("Index");
-    //    }
-
-    //    var client = _clientFactory.CreateClient();
-
-    //    // Konverter listen af tal til en streng, der kan inkluderes i en URL som forespørgselsparametre
-    //    var queryString = string.Join("&input=", numbers.Prepend(0)); // Bruger Prepend(0) som en hurtig måde at tilføje et element først i listen og anvende samme formattering gennem Join.
-
-    //    var result = await client.GetStringAsync($"http://subing-service/Sub?{queryString}");
-
-    //    ViewBag.Result = result;
-    //    return View("Index");
-    //}
+    
 }
